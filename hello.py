6044a43a5c0c5f181489f1da8826788780c28ae0
Lais Gabriele <161658694+laisgsz@users.noreply.github.com>
2025-10-23 20:38:17 -0300

    Refactor email handling and update routes

--- hello.py
+++ hello.py
@@ -1,60 +1,3 @@
-import os
-import sys
-from threading import Thread
-from flask import Flask, render_template, session, redirect, url_for
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_mail import Mail, Message
-
-import requests
-from datetime import datetime
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-app.config['API_KEY'] = os.environ.get('API_KEY')
-app.config['API_URL'] = os.environ.get('API_URL')
-app.config['API_FROM'] = os.environ.get('API_FROM')
-
-app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
-app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
-
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-mail = Mail(app)
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
 class Email(db.Model):
     __tablename__ = 'emails'
     id = db.Column(db.Integer, primary_key=True)
@@ -77,7 +20,7 @@ def send_simple_message(to, subject, newUser):
                              auth=("api", app.config['API_KEY']), data={"from": app.config['API_FROM'], 
                                                                         "to": to, 
                                                                         "subject": app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, 
-                                                                        "text": "Prontuário: PT3025993\nNome: Lais Gabriele Da Silva\nNovo usuário cadastrado: " + newUser})
+                                                                        "text": "Prontuário: PT3025993\nNome: Lais Gabriele Da Silva \nNovo usuário cadastrado: " + newUser})
     print('Enviando mensagem (Resposta)...' + str(resposta) + ' - ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), flush=True)
     email = Email(remetente = newUser, destinatario = str(to), assunto = str(app.config['FLASKY_MAIL_SUBJECT_PREFIX']) + ' ' + subject, corpo = "Prontuário: PT303304X\nNome: Giovanna Karolline Menezes Ribeiro\nNovo usuário cadastrado: " + newUser, data = datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
     db.session.add(email)
@@ -104,10 +47,11 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/enviaremail')
-def enviaremail():
+@app.route('/emailsEnviados')
+def emailsEnviados():
     emails = Email.query.all()
-    return render_template('enviaremail.html', emails = emails)
+    return render_template('emailsEnviados.html', emails = emails)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
